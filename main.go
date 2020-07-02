15d03c40a81b9f2ad6942e657a4cc1152db53955
petderek <petderek@amazon.com>
2020-07-02 12:52:42 -0700

    syscall -> x/sys/unix
    This library has a much nicer pattern that requires no crazy transcribe
    function.

--- main.go
+++ main.go
@@ -2,30 +2,20 @@ package main
 
 import (
 	"log"
-	"syscall"
+
+	"golang.org/x/sys/unix"
 )
 
 func main() {
-	var uname syscall.Utsname
-	if err := syscall.Uname(&uname); err != nil {
+	var uname unix.Utsname
+	if err := unix.Uname(&uname); err != nil {
 		log.Fatal("syscall failed: ", err)
 	}
 
-	log.Println("Sysname:", transcribe(uname.Sysname[:]))
-	log.Println("Nodename:", transcribe(uname.Nodename[:]))
-	log.Println("Release:", transcribe(uname.Release[:]))
-	log.Println("Version:", transcribe(uname.Version[:]))
-	log.Println("Machine:", transcribe(uname.Machine[:]))
-	log.Println("Domainname:", transcribe(uname.Domainname[:]))
-}
-
-func transcribe(val []int8) string {
-	var buf [65]byte
-	for i, v := range val {
-		if v == 0 {
-			break
-		}
-		buf[i] = uint8(v)
-	}
-	return string(buf[:])
+	log.Println("Sysname:", string(uname.Sysname[:]))
+	log.Println("Nodename:", string(uname.Nodename[:]))
+	log.Println("Release:", string(uname.Release[:]))
+	log.Println("Version:", string(uname.Version[:]))
+	log.Println("Machine:", string(uname.Machine[:]))
+	log.Println("Domainname:", string(uname.Domainname[:]))
 }
